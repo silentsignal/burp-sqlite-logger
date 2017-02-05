35b800e1a4dd60783d83a560cd89063f82565aa8
András Veres-Szentkirályi <vsza@vsza.hu>
2017-02-05 12:51:26 +0100

    added message editors (#1)

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -13,9 +13,11 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.table.*;
 
-public class BurpExtender extends JPanel implements IBurpExtender, ITab, IHttpListener {
+public class BurpExtender extends JPanel implements IBurpExtender, ITab,
+	   IHttpListener, IMessageEditorController, ListSelectionListener {
 
 	private IBurpExtenderCallbacks callbacks;
 	private IExtensionHelpers helpers;
@@ -25,6 +27,7 @@ public class BurpExtender extends JPanel implements IBurpExtender, ITab, IHttpLi
 	private JLabel lbDbFile = new JLabel("(no database opened yet)");
 	private JTextArea filters = new JTextArea();
 	private OutputStream stderr;
+	private IMessageEditor requestViewer, responseViewer;
 
 	@Override
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
@@ -86,9 +89,64 @@ public class BurpExtender extends JPanel implements IBurpExtender, ITab, IHttpLi
 		toolbar.add(databaseControls);
 		toolbar.add(filtersControls);
 
+		// -------- split pane --------
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JTabbedPane tabs = new JTabbedPane();
+		table.getSelectionModel().addListSelectionListener(this);
+		splitPane.setTopComponent(new JScrollPane(table));
+		splitPane.setBottomComponent(tabs);
+
+		requestViewer = callbacks.createMessageEditor(this, false);
+		responseViewer = callbacks.createMessageEditor(this, false);
+		tabs.addTab("Request", requestViewer.getComponent());
+		tabs.addTab("Response", responseViewer.getComponent());
+
 		setLayout(new BorderLayout());
 		add(toolbar, BorderLayout.NORTH);
-		add(new JScrollPane(table), BorderLayout.CENTER);
+		add(splitPane, BorderLayout.CENTER);
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		requestViewer.setMessage(getRequest(), true);
+		responseViewer.setMessage(getResponse(), false);
+	}
+
+	@Override
+	public IHttpService getHttpService() {
+		Integer id = getSelectedId();
+		if (id == null) return null;
+		try (PreparedStatement ps = conn.prepareStatement(
+					"SELECT host, port, protocol FROM messages WHERE id = ?")) {
+			ps.setInt(1, id);
+			try (ResultSet rs = ps.executeQuery()) {
+				if (!rs.next()) return null;
+				final String host = rs.getString(1);
+				final int port = rs.getInt(2);
+				final String protocol = rs.getString(3);
+				return new IHttpService() {
+					public String getHost() { return host; }
+					public int getPort() { return port; }
+					public String getProtocol() { return protocol; }
+				};
+			}
+		} catch (SQLException e) {
+			reportError(e, null);
+			return null;
+		}
+	}
+
+	@Override
+	public byte[] getRequest()  { return getSelectedMsgBytes( "request"); }
+
+	@Override
+	public byte[] getResponse() { return getSelectedMsgBytes("response"); }
+
+	private Integer getSelectedId() {
+		int selectedRow = table.getSelectedRow();
+		if (selectedRow == -1) return null;
+		return (Integer)table.getValueAt(selectedRow, 0);
 	}
 
 	private void reportError(Throwable t, String title) {
@@ -197,6 +255,19 @@ public class BurpExtender extends JPanel implements IBurpExtender, ITab, IHttpLi
 	private Object getMsgInt(String field, Integer id) throws SQLException {
 		try (ResultSet rs = getMsgField(field, id)) {
 			return rs.next() ? rs.getInt(1) : "";
+		}
+	}
+
+	private static final byte[] EMPTY_BYTE_ARRAY = {};
+
+	private byte[] getSelectedMsgBytes(String field) {
+		Integer id = getSelectedId();
+		if (id == null) return EMPTY_BYTE_ARRAY;
+		try (ResultSet rs = getMsgField(field, id)) {
+			return rs.next() ? rs.getBytes(1) : EMPTY_BYTE_ARRAY;
+		} catch (SQLException e) {
+			reportError(e, null);
+			return EMPTY_BYTE_ARRAY;
 		}
 	}
 
